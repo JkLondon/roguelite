1de882dbc6bf6b70b16bfa265051bc9965dea5e2
Ilya Danilin <danilin.iv@phystech.edu>
2019-12-08 21:16:32 +0300

    Replace right and left.

--- lib_sprites.py
+++ lib_sprites.py
@@ -5,15 +5,15 @@ list_mob_stand = []
 list_mob_stand.append('test_mob_stand(1).png')
 list_mob_stand.append('test_mob_stand(2).png')
 list_mob_go_left = []
-list_mob_go_left.append('test_mob_go_right(1).png')
-list_mob_go_left.append('test_mob_go_right(2).png')
+list_mob_go_left.append('test_mob_go_left(1).png')
+list_mob_go_left.append('test_mob_go_left(2).png')
 list_mob_go_right = []
-list_mob_go_right.append('test_mob_go_left(1).png')
-list_mob_go_right.append('test_mob_go_left(2).png')
+list_mob_go_right.append('test_mob_go_right(1).png')
+list_mob_go_right.append('test_mob_go_right(2).png')
 
 stand = create_sprite(list_mob_stand,timer=15)
-go_left = create_sprite(list_mob_go_left,timer=1)
-go_right = create_sprite(list_mob_go_right,timer=1)
+go_left = create_sprite(list_mob_go_left,timer=5)
+go_right = create_sprite(list_mob_go_right,timer=5)
 
 TEST_MOB = {'stand': stand, 'go_left': go_left, 'go_right': go_right}
 
